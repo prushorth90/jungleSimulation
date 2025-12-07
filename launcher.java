ae7fa95b01987039f634b0206d829320285a5de8
prushorth <maniprush3@gmail.com>
2025-12-06 23:42:42 -0800

    Add animal simulation

---
+++ launcher.java
@@ -0,0 +1,7 @@
+public class Launcher {
+    public static void main(String[] args) {
+        Simulator sim = new Simulator();
+        // This command actually starts the loop
+        sim.runLongSimulation(); 
+    }
+}
